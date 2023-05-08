6c9e62868969ab7da2654440c40c8f5571e51d1c
walterdn03 <131649543+walterdn03@users.noreply.github.com>
2023-05-08 12:12:36 +0200

    Update sketch.js

--- Esercizio_3/1_disegno/sketch.js
+++ Esercizio_3/1_disegno/sketch.js
@@ -3,5 +3,25 @@ function setup(){
 }
 
 function draw(){
-	background(255,0,0)
+	background(200)
+
+	point(30,50)  //istruzione +  parametri
+	point(0,0)
+	point(31,50)
+
+	strokeWeight(7)
+	fill(255,0,0)	//imposto fill prima della forma
+	rect(100,120, 100,100)	//all'inizio xy e poi base + altezza
+	
+	line(50,60, 200,280)	//xy punto di partenza + xy punto di arrivo
+	
+	strokeWeight(3)
+	rect(150,150, 100,100)
+
+	//in base a come scrivo il codice visualizzerò prima una cosa e poi l'altra
+	fill(0,190,220, 128) //ultimo parametro è opacità
+	ellipse(180,280, 90,90)
+
+	noFill()
+	triangle(310,90, 390,160, 210,180)
 }
